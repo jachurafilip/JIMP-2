3a820a13f2bf1f6100bb9d5ffd4d276230b8c316
filjac <filjac5882@op.pl>
2018-03-02 22:18:11 +0100

    Palindrome solved

--- lab1/palindrome/main.cpp
+++ lab1/palindrome/main.cpp
@@ -1,4 +1,27 @@
-//
-// Created by filip on 02.03.18.
-//
+#include "Palindrome.h"
+#include <string>
+#include <iostream>
 
+using namespace std;
+int main()
+{
+    cout <<"1 - Sprawdz palindrom\n";
+    cout <<"2 - Wyjscie\n\n";
+
+    char key;
+    string slowo;
+    cin >> key;
+    while (key != '2')
+    {
+        if (key == '1')
+        {
+            cout << "Podaj slowo: \n";
+            cin >> slowo;
+            cout << is_palindrome(slowo) << endl;
+        }
+    cout <<"1 - Sprawdz palindrom\n";
+    cout <<"2 - Wyjscie\n\n";
+    cin >> key;
+    }
+
+}
